0bc2981a24eea7889ba7ec463375040e2fbce658
whoisthisadam <90774858+whoisthisadam@users.noreply.github.com>
2021-09-30 23:42:43 +0300

    задание 1

---
+++ номер11cpp.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<locale>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int n;
+    int n1 = 0;
+    cout << "¬ведите число:"; cin >> n;
+    if (n < 0) n = -n;
+    if (n == 0) cout << 0 << endl;
+    else
+    {
+        while (n >= 1)
+        {
+            n1 = 10 * n1 + n % 10;
+            n /= 10;
+        }
+
+        while (n1 >= 1)
+        {
+            cout << n1 % 10 << endl;
+            n1 /= 10;
+        }
+    }
+    return 0;
+}
